b8f2f0a23087ef178172de5de7681a404e1224e5
LsK1z <jairolucasbs22@outlook.com>
2024-08-26 15:59:38 -0300

    ajuste de index

---
+++ assets/js/scripts.js
@@ -0,0 +1,45 @@
+document.addEventListener("DOMContentLoaded", function() {
+    // Lista de arquivos das atividades
+    const atividades = [
+        '/AT1/index.html',
+        '/AT2/index.html',
+        '/AT3/index.html',
+        '/AT4/index.html',
+        '/AT5/index.html',
+        '/AT6/index.html',
+        '/AT7/index.html',
+        '/AT8/index.html',
+        '/AT9/index.html',
+        '/AT10/index.html',
+        '/AT11/index.html',
+        '/AT12/index.html',
+        '/AT13/index.html',
+        '/AT14/index.html',
+        '/AT15/index.html',
+        '/AT16/index.html',
+        '/AT17/index.html',
+        '/AT18/index.html',
+        '/AT19/index.html',
+    ];
+
+    const atividadesContainer = document.getElementById('atividades-container');
+
+    atividades.forEach((atividade, index) => {
+        const button = document.createElement('button');
+        button.type = 'button';
+        button.className = 'btn btn-light';
+        button.textContent = `ATIVIDADE ${index + 1}`;
+        button.onclick = () => window.location.href = `atividades/${atividade}`;
+        atividadesContainer.appendChild(button);
+    });
+});
+
+document.addEventListener("DOMContentLoaded", function() {
+    const boxAtividades = document.querySelector(".boxAtividades");
+    const atividadesLink = document.querySelector("nav a:nth-child(3)");
+    
+    atividadesLink.addEventListener("click", function() {
+        boxAtividades.classList.toggle("visible");
+    });
+});
+
